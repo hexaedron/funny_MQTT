a1f5a6c2bd199b41a9545b24c0d3995cad7f70eb
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2025-03-02 13:17:56 +0300

    No need for template anymore + cleanup

--- include/MQTTClient.h
+++ include/MQTTClient.h
@@ -10,21 +10,14 @@ enum eMQTTTopicTypes
 };
 
 
-//template <uint8_t pubTopicCount, uint8_t subTopicCount>
 class MQTTClient : public tcpClient
 {
 private:
-    //char* pubTopics[pubTopicCount];
-    //char* subTopics[subTopicCount];
     uint8_t currentPubTopic = 0;
     uint8_t currentSubTopic = 0;
     uint16_t MQTTPackedID = 5;
-    //int pub_qos = 0;                                   //Publish quality of service
-    //int sub_qos = 0;                                   //Subscription quality of service
 public:
     MQTTClient(ethIF* eth, uint8_t* newDestIPAddress, uint16_t newDestIPPort = 1883): tcpClient(eth, newDestIPAddress, newDestIPPort) {};
-    //bool pushTopic(eMQTTTopicTypes type, const char* topic);
-    //void setQOS(int newPubQOS, int newSubQOS);
     void MQTTConnect(char *username = nullptr, char *password = nullptr);
     void MQTTSubscribe( char *topic, int req_qos);
     void MQTTUnsubscribe(char *topic);
--- src/MQTTClient.cpp
+++ src/MQTTClient.cpp
@@ -1,39 +1,5 @@
 #include "MQTTClient.h"
 
-//template<uint8_t pubTopicCount, uint8_t subTopicCount>
-//bool MQTTClient<pubTopicCount, subTopicCount>::pushTopic(eMQTTTopicTypes type, const char* topic)
-//{
-//    if(type == eMQTTTopicTypes::PUB)
-//    {
-//        if(this->currentPubTopic >= pubTopicCount)
-//        {
-//            return false;
-//        }
-//        this->pubTopics[this->currentPubTopic] = topic;
-//        this->currentPubTopic++;
-//    }
-//    else
-//    {
-//        if(this->currentSubTopic >= subTopicCount)
-//        {
-//            return false;
-//        }
-//        this->subTopics[this->currentSubTopic] = topic;
-//        this->currentSubTopic++;
-//    }
-//
-//    return true;
-//}
-
-//template<uint8_t pubTopicCount, uint8_t subTopicCount>
-//void MQTTClient<pubTopicCount, subTopicCount>::setQOS(int newPubQOS, int newSubQOS)
-//{
-//    this->pub_qos = newPubQOS;
-//    this->sub_qos = newSubQOS;
-//}
-
-//template<uint8_t pubTopicCount, uint8_t subTopicCount>
-//void MQTTClient<pubTopicCount, subTopicCount>::MQTTConnect(char *username, char *password)
 void MQTTClient::MQTTConnect(char *username, char *password)
 {
     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
@@ -54,9 +20,8 @@ void MQTTClient::MQTTConnect(char *username, char *password)
     len = MQTTSerialize_connect(buf, sizeof(buf), &data);
     this->sendPacket(buf, len);
 }
-/*
-template<uint8_t pubTopicCount, uint8_t subTopicCount>
-void MQTTClient<pubTopicCount, subTopicCount>::MQTTSubscribe( char *topic, int req_qos)
+
+void MQTTClient::MQTTSubscribe( char *topic, int req_qos)
 {
     MQTTString topicString = MQTTString_initializer;
     u32 len;
@@ -68,8 +33,7 @@ void MQTTClient<pubTopicCount, subTopicCount>::MQTTSubscribe( char *topic, int r
     this->sendPacket(buf, len);
 }
 
-template<uint8_t pubTopicCount, uint8_t subTopicCount>
-void MQTTClient<pubTopicCount, subTopicCount>::MQTTUnsubscribe(char *topic)
+void MQTTClient::MQTTUnsubscribe(char *topic)
 {
     MQTTString topicString = MQTTString_initializer;
     u32 len;
@@ -80,10 +44,7 @@ void MQTTClient<pubTopicCount, subTopicCount>::MQTTUnsubscribe(char *topic)
     len = MQTTSerialize_unsubscribe(buf, sizeof(buf), 0, msgid, 1, &topicString);
     this->sendPacket(buf, len);
 }
-*/
 
-//template<uint8_t pubTopicCount, uint8_t subTopicCount>
-//void MQTTClient<pubTopicCount, subTopicCount>::MQTTPublish(char *topic, int qos, char *payload)
 void MQTTClient::MQTTPublish(char *topic, int qos, char *payload)
 {
     MQTTString topicString = MQTTString_initializer;
@@ -96,9 +57,8 @@ void MQTTClient::MQTTPublish(char *topic, int qos, char *payload)
     len = MQTTSerialize_publish(buf, sizeof(buf), 0, qos, 0, this->MQTTPackedID++, topicString, (unsigned char*)payload, payloadlen);
     this->sendPacket(buf, len);
 }
-/*
-template<uint8_t pubTopicCount, uint8_t subTopicCount>
-void MQTTClient<pubTopicCount, subTopicCount>::MQTTPingreq(void)
+
+void MQTTClient::MQTTPingreq(void)
 {
     u32 len;
     u8 buf[10];
@@ -107,12 +67,10 @@ void MQTTClient<pubTopicCount, subTopicCount>::MQTTPingreq(void)
     this->sendPacket(buf, len);
 }
 
-template<uint8_t pubTopicCount, uint8_t subTopicCount>
-void MQTTClient<pubTopicCount, subTopicCount>::MQTTDisconnect(void)
+void MQTTClient::MQTTDisconnect(void)
 {
     u32 len;
     u8 buf[50];
     len = MQTTSerialize_disconnect(buf,sizeof(buf));
     this->sendPacket(buf, len);
 }
-    */
